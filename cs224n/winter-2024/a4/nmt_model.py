8c948a5c87a8d87334ed749ae7f27c0c7fe8b3f7
Pawel Golinski <golpaw1@gmail.com>
2026-01-25 02:21:54 +0000

    cs224n a4: sanity check 1e

--- cs224n/winter-2024/a4/nmt_model.py
+++ cs224n/winter-2024/a4/nmt_model.py
@@ -270,7 +270,16 @@ class NMT(nn.Module):
         ###     Tensor Stacking:
         ###         https://pytorch.org/docs/stable/torch.html#torch.stack
 
-
+        enc_hiddens_proj = self.att_projection(enc_hiddens)
+        Y = self.model_embeddings.target(target_padded)
+        Y_split = torch.split(Y, 1, dim=0)
+        for Y_t in Y_split:
+            Y_t = Y_t.squeeze(0)
+            Ybar_t = torch.cat([Y_t, o_prev], dim=-1)
+            dec_state, o_t, _ = self.step(Ybar_t, dec_state, enc_hiddens, enc_hiddens_proj, enc_masks)
+            combined_outputs.append(o_t)
+            o_prev = o_t
+        combined_outputs = torch.stack(combined_outputs, dim=0)
 
         ### END YOUR CODE
 
